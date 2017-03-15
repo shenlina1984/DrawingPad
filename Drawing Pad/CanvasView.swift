1dbb90ef411364018466826b8a2f48a939c61c59
yzhaoINT <yzhao@ideanovatech.com>
2017-03-14 19:39:28 -0500

    update some features

--- Drawing Pad/CanvasView.swift
+++ Drawing Pad/CanvasView.swift
@@ -25,7 +25,7 @@ struct Scribbles{
 }
 var undoList: [Scribbles]=[]
 //let context=UIGraphicsGetCurrentContext()
-class CanvasView: UIView {
+class CanvasView: UIView{
     var width: CGFloat=10.0
     var color_config = UIColor(displayP3Red: 1.0, green: 0.0, blue: 0.0, alpha: 1.0)
     //var shape: ShapeType = .Line
@@ -43,7 +43,7 @@ class CanvasView: UIView {
         // Drawing code
         if let context = UIGraphicsGetCurrentContext() {
             if(deleteFlag==false){
-               
+                
                 if undoList.count>0{
                     for i in 0...undoList.count-1{
                         context.setLineWidth(undoList[i].width)
@@ -56,7 +56,7 @@ class CanvasView: UIView {
                     }
                     if drawAvailable{
                         color_scr=color_config;
-
+                        
                     }
                     if eraseAvailable{
                         color_scr=self.backgroundColor!
@@ -68,7 +68,7 @@ class CanvasView: UIView {
                         context.addLine(to: CGPoint(x: p.x, y: p.y))
                     }
                     context.strokePath()
-
+                    
                 }
                 else{
                     if drawAvailable{
@@ -85,7 +85,7 @@ class CanvasView: UIView {
                         context.addLine(to: CGPoint(x: p.x, y: p.y))
                     }
                     context.strokePath()
-
+                    
                 }
             }
             else{
@@ -93,83 +93,96 @@ class CanvasView: UIView {
                 if !undoList.isEmpty {
                     undoDeleteFlag=true
                 }
-
-                            }
+                
+            }
         }
         
     }
+    
+    
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let touch = touches.first {
-            if let t1=alpha_s{
-                if let t2=selectedColor{
-                    let colors = t2.cgColor.components
-                color_config=UIColor(displayP3Red: colors![0], green: colors![1], blue: colors![2], alpha: t1)
-
-            }
-                else{
-                    color_config=UIColor(displayP3Red: 1.0, green: 0.0, blue: 0.0, alpha: t1)
-                }
-            }
-            else{
-                if let t2=selectedColor{
-                    let colors = t2.cgColor.components
-                    color_config=UIColor(displayP3Red: colors![0], green: colors![1], blue: colors![2], alpha: 1.0)
-                    
+        if (touches.count == 1){
+            if let touch = touches.first {
+                if let t1=alpha_s{
+                    if let t2=selectedColor{
+                        let colors = t2.cgColor.components
+                        color_config=UIColor(displayP3Red: colors![0], green: colors![1], blue: colors![2], alpha: t1)
+                        
+                    }
+                    else{
+                        color_config=UIColor(displayP3Red: 1.0, green: 0.0, blue: 0.0, alpha: t1)
+                    }
                 }
                 else{
-                    color_config=UIColor(displayP3Red: 1.0, green: 0.0, blue: 0.0, alpha: 1.0)
+                    if let t2=selectedColor{
+                        let colors = t2.cgColor.components
+                        color_config=UIColor(displayP3Red: colors![0], green: colors![1], blue: colors![2], alpha: 1.0)
+                        
+                    }
+                    else{
+                        color_config=UIColor(displayP3Red: 1.0, green: 0.0, blue: 0.0, alpha: 1.0)
+                    }
                 }
+                
+                if let t=width_s{
+                    width=t
+                }
+                first = touch.location(in: self)
+                last = first
+                if (drawAvailable||eraseAvailable){
+                    points.append(first)
+                    //setNeedsDisplay()
+                }
+                
+                
             }
-            
-            if let t=width_s{
-                width=t
-            }
-            first = touch.location(in: self)
-            last = first
-            if (drawAvailable||eraseAvailable){
-                points.append(first)
-                setNeedsDisplay()
-            }
-            
-            
         }
+
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
-        if let touch = touches.first {
+        if (touches.count == 1) {
             
-            last = touch.location(in: self)
-             if (drawAvailable||eraseAvailable){
-               
-                points.append(last)
-       
-                setNeedsDisplay()
+            if let touch = touches.first {
+                
+                last = touch.location(in: self)
+                if (drawAvailable||eraseAvailable){
+                    
+                    points.append(last)
+                    
+                    setNeedsDisplay()
+                }
+                
             }
-       
         }
+
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let touch = touches.first {
-            
+        if (touches.count == 1) {
+            if let touch = touches.first {
+                
                 last = touch.location(in: self)
-            if (drawAvailable||eraseAvailable){
-                points.append(last)
-                let path=points
-                points.removeAll(keepingCapacity: true)
-                setNeedsDisplay()
-                if drawAvailable{
-                    color_scr=color_config;
-                    
+                if (drawAvailable||eraseAvailable){
+                    points.append(last)
+                    let path=points
+                    points.removeAll(keepingCapacity: true)
+                    setNeedsDisplay()
+                    if drawAvailable{
+                        color_scr=color_config;
+                        
+                    }
+                    if eraseAvailable{
+                        color_scr=self.backgroundColor!
+                    }
+                    undoList.append(Scribbles(c:color_scr,p:path,w:width))
                 }
-                if eraseAvailable{
-                    color_scr=self.backgroundColor!
-                }
-            undoList.append(Scribbles(c:color_scr,p:path,w:width))
+                
             }
-        
         }
+        
+       
         
     }
   
--- Drawing Pad/DataViewController.swift
+++ Drawing Pad/DataViewController.swift
@@ -8,12 +8,48 @@
 
 import UIKit
 
-class DataViewController: UIViewController {
+class DataViewController: UIViewController,UITextFieldDelegate {
     
     var color:UIColor!
     @IBOutlet weak var canvas: CanvasView!
     @IBOutlet weak var brush: UIButton!
+    @IBAction func backGroundTouch(_ sender: UIControl) {
+        imgTitle.resignFirstResponder()
+    }
+    
+    @IBAction func editingDidEnd(_ sender: UITextField) {
+        sender.resignFirstResponder()
+    }
     @IBOutlet weak var erase: UIButton!
+    @IBOutlet weak var imgTitle: UITextField!
+    
+
+
+    @IBAction func newProject(_ sender: UIButton) {
+        let alertController = UIAlertController(title: "Alert", message: "Do you want to save current drawing to image?", preferredStyle: .alert)
+        
+        // Create the actions
+        let okAction = UIAlertAction(title: "Yes", style: UIAlertActionStyle.default) {
+            UIAlertAction in
+            self.saveToDisk()
+            while(!undoList.isEmpty){
+                undoList.removeAll()
+            }
+            self.canvas.setNeedsDisplay()
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.cancel) {
+            UIAlertAction in
+            NSLog("Cancel Pressed")
+            while(!undoList.isEmpty){
+                undoList.removeAll()
+            }
+            self.canvas.setNeedsDisplay()
+        }
+        alertController.addAction(okAction)
+        alertController.addAction(cancelAction)
+        // Present the controller
+        self.present(alertController, animated: true, completion: nil)
+        }
 
     @IBAction func pickPen(_ sender: UIButton) {
         switch sender {
@@ -73,13 +109,94 @@ class DataViewController: UIViewController {
         }, completion: nil)
     }
     
+    func saveToDisk(){
+        
+        UIGraphicsBeginImageContextWithOptions(canvas.bounds.size, false, 0.0)
+        canvas.drawHierarchy(in: view.bounds, afterScreenUpdates: true)
+        let img = UIGraphicsGetImageFromCurrentImageContext();
+        UIGraphicsEndImageContext();
+        
+        UIImageWriteToSavedPhotosAlbum(img!, self, #selector(image(_:didFinishSavingWithError:contextInfo:)), nil)
+    }
+    
+    @IBAction func shareImg(){
+        UIGraphicsBeginImageContextWithOptions(canvas.bounds.size, false, 0.0)
+        canvas.drawHierarchy(in: view.bounds, afterScreenUpdates: true)
+        let img = UIGraphicsGetImageFromCurrentImageContext();
+        UIGraphicsEndImageContext();
+
+        let shareText = "Share my drawing work"
+        
+        let vc = UIActivityViewController(activityItems: [shareText, img!], applicationActivities: [])
+        present(vc, animated: true)
+    }
+    
+    @IBAction func clickSave(){
+        // Create the alert controller
+        let alertController = UIAlertController(title: "Alert", message: "Do you want to save to this drawing to image?", preferredStyle: .alert)
+        
+        // Create the actions
+        let okAction = UIAlertAction(title: "Yes", style: UIAlertActionStyle.default) {
+            UIAlertAction in
+            self.saveToDisk()
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.cancel) {
+            UIAlertAction in
+            NSLog("Cancel Pressed")
+        }
+        
+        // Add the actions
+        alertController.addAction(okAction)
+        alertController.addAction(cancelAction)
+        
+        // Present the controller
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    func image(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        if let error = error {
+            // we got back an error!
+            let ac = UIAlertController(title: "Save error", message: error.localizedDescription, preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        } else {
+            let ac = UIAlertController(title: "Saved!", message: "Your image has been saved to your photos.", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        }
+    }
+    
+    @IBAction func handlePinch(recognizer : UIPinchGestureRecognizer) {
+        if let view = recognizer.view {
+            view.transform = view.transform.scaledBy(x: recognizer.scale, y: recognizer.scale)
+            recognizer.scale = 1
+        }
+    }
+    
+    func textFieldShouldReturn(textField: UITextField!) -> Bool // called when 'return' key pressed. return NO to ignore.
+    {
+        imgTitle.resignFirstResponder()
+        return true;
+    }
+
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-       
+       imgTitle.text="New Image"
+        
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
+        view.addGestureRecognizer(tap)
+        
+
         
         
+    }
+    
+    func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
     }
 
     override func didReceiveMemoryWarning() {
